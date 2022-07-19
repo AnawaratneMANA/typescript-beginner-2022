a14bb320a3e0e175d714e8aa479435850681aeb1
AnawaratneMANA <it19086408@my.sliit.lk>
2022-07-19 13:07:46 +0530

    COMMIT: Function return types.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-const enum Size { Small = 1, Medium = 4, Large}
-let mySize: Size = Size.Large
-console.log(mySize)
+function calculateTax(income: number): number{
+    return 0;
+}
+
+// return type should be mentioned. We can't return anything other than
+// number.
