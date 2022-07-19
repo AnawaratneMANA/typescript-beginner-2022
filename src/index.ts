15bbdaf39d7150cfab9786eb6e1672fdfffca682
AnawaratneMANA <it19086408@my.sliit.lk>
2022-07-19 13:15:10 +0530

    COMMIT: Optional parameters in objects.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-function calculateTax(income: number): number{
-   if(income < 50000){
-    return 1;
-   } else {
-    return 2;
-   }
-}
-
-function calculateTax2(income: number, tax=399): number{
-    if(income < 50000){
-     return 1;
-    } else {
-     return 2;
-    }
- }
-
-calculateTax(10_000) // Default parameters are no need to mentioned.
-
-// return type should be mentioned. We can't return anything other than
-// number.
+let employee: {
+    id: number,
+    name?: string
+} = {id: 1, }
+employee.name = 'Akash';
