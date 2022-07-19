1ee8b134fb84c38eb3ad525fe896f14a3672db04
AnawaratneMANA <it19086408@my.sliit.lk>
2022-07-19 13:12:40 +0530

    COMMIT: Default parameters.

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,15 @@ function calculateTax(income: number): number{
    }
 }
 
+function calculateTax2(income: number, tax=399): number{
+    if(income < 50000){
+     return 1;
+    } else {
+     return 2;
+    }
+ }
+
+calculateTax(10_000) // Default parameters are no need to mentioned.
+
 // return type should be mentioned. We can't return anything other than
 // number.
